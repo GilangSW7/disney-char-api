94618023221f80fa99d15c5c1d97edcbbcc2435b
GilangSW7 <gilangwidianto117@gmail.com>
2022-08-15 04:21:03 +0700

    delete not use import in ErrorHandlerCode

--- src/main/java/com/srin/assignment/disneycharapi/exception/handler/ErrorHandler.java
+++ src/main/java/com/srin/assignment/disneycharapi/exception/handler/ErrorHandler.java
@@ -1,27 +1,14 @@
 package com.srin.assignment.disneycharapi.exception.handler;
-
-import com.srin.assignment.disneycharapi.enums.ResponseEnum;
 import com.srin.assignment.disneycharapi.exception.CustomException;
 import com.srin.assignment.disneycharapi.exception.response.ErrorResponse;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-/**
- * Created by oky.suryadi on 23/04/21.
- */
+
 @RestControllerAdvice
 public class ErrorHandler {
 
